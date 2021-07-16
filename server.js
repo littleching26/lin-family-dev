24fcba0c99397ae3fe4c820a70ab54d591c52e9c
YiChing <littleching26@gmail.com>
2021-07-16 17:40:48 +0800

    revert  package.json

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/lin-family-daily'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/lin-family-daily/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
