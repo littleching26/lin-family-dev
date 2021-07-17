a3c043e02a456cfa0ac05fdedb54cd8a787be3f9
YiChing <littleching26@gmail.com>
2021-07-17 15:35:49 +0800

    change to 80 port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path')
-const port = 8080;
+const port = 80;
 const app = express();
 
 app.use(express.static('./dist/lin-family-daily'));
@@ -9,6 +9,6 @@ app.get('/', function(req, res) {
     res.sendFile('index.html',{root:__dirname});
 });
 
-app.listen(8080, () => {
-    console.log("Server is listening on port "+8080);
+app.listen(port, () => {
+    console.log("Server is listening on port "+port);
 });
